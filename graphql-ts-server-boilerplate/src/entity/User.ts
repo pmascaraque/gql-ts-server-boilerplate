bbd6cb522b0fc4eff4f7db1a66efc99f29381e44
Pablo Moreno <ssm.pablo@gmail.com>
2022-08-16 18:38:24 +0200

    user entity

--- graphql-ts-server-boilerplate/src/entity/User.ts
+++ graphql-ts-server-boilerplate/src/entity/User.ts
@@ -1,2 +1,24 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, Column, BeforeInsert, PrimaryColumn, BaseEntity } from "typeorm"
+import { v4 as uuidv4 } from "uuid";//????
 
+@Entity()
+export class User extends BaseEntity {
+
+    @PrimaryColumn("uuid") id: string;
+
+    @Column("varchar", {length: 255}) email: string;
+
+    @Column("text") password: string;
+
+    @BeforeInsert()
+    addId() {
+        this.id = uuidv4();
+    }
+
+    constructor(id: string, email: string, password: string) {
+        super();
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+  }
